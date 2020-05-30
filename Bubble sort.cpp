0a86d56835f2126df168cbba5f3ca12a37689500
Diana Ivanchevska <ivanchevska.diana@gmail.com>
2020-05-30 16:30:46 +0300

    + for

--- Bubble sort.cpp
+++ Bubble sort.cpp
@@ -4,9 +4,14 @@ using namespace std;
 int main()
 {
 	const int m = 10;
-	int arr[m] = { 5, 19,27,3,8,1, 4, 1, 4, 11 };
-	cout << "Before:\n" << arr[0] << " " << arr[1] << " " << arr[2] << " " << arr[3] << " " << arr[4] << " " << arr[5] << " " << arr[6] << " " << arr[7] << " " << arr[8] << " " << arr[9]  << endl;
 	int j, s, i;
+	int arr[m] = { 5, 19,27,3,8,1, 4, 1, 4, 11 };
+	cout << "Before:\n";
+	for (i = 0; i < m; i++)
+	{
+		cout << arr[i] << " ";
+	}
+
 	for (j = 1; j <= (m - 1); j++)
 	{
 		for (i=0; i < m-j; i++)
@@ -19,7 +24,11 @@ int main()
 		}
 	}
 	cout << "\nAfter:\n";
-	cout <<  arr[0] << " " << arr[1] << " " << arr[2] << " " << arr[3] << " " << arr[4] << " " << arr[5] << " " << arr[6] << " " << arr[7] << " " << arr[8] << " " << arr[9] << endl;
+	for (i = 0; i < m; i++)
+	{
+		cout << arr[i] << " ";
+	}
+	cout << "\n";
 	return 0;
    
 }
